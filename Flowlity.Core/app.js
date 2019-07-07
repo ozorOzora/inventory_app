435c2c32a7b8b0409210952ba56d0751c5b9a0ae
Elias Ousghir <ousghir.elias@gmail.com>
2019-07-07 14:37:45 +0200

    Cleanup

--- Flowlity.Core/app.js
+++ Flowlity.Core/app.js
@@ -1,76 +1,64 @@
-﻿//initializing in-memory database and collections
-import * as Loki from 'lokijs';
-import * as Path from 'path';
-import { Product } from "../Flowlity.Core/BO/product";
-import { Availability } from "../Flowlity.Core/BO/availability";
-import * as Debug from 'debug';
-import * as Express from 'express';
-import routes from '../Flowlity.Core/routes/index';
-
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+//initializing in-memory database and collections
+var Loki = require("lokijs");
+var Path = require("path");
+var Debug = require("debug");
+var Express = require("express");
+var index_1 = require("./routes/index");
 var inMemoryDb = new Loki('data.json');
-
 var productsCol = inMemoryDb.addCollection('products');
-var productRepository = require( '../Flowlity.Core/DAL/product.repository')(productsCol);
-var productManager = require('../Flowlity.Core/BLL/product.manager')(productRepository);
-var productController = require('../Flowlity.Core/Controller/product.controller')(productManager);
-
+var productRepository = require('./DAL/product.repository')(productsCol);
+var productManager = require('./BLL/product.manager')(productRepository);
+var productController = require('./Controller/product.controller')(productManager);
 var availabilitiesCol = inMemoryDb.addCollection('availabilities');
-var availabilityRepository = require('../Flowlity.Core/DAL/availability.repository')(availabilitiesCol);
-var availabilityManager = require('../Flowlity.Core/BLL/availability.manager')(availabilityRepository);
-var availabilityController = require('../Flowlity.Core/Controller/availability.controller')(availabilityManager);
-
+var availabilityRepository = require('./DAL/availability.repository')(availabilitiesCol);
+var availabilityManager = require('./BLL/availability.manager')(availabilityRepository);
+var availabilityController = require('./Controller/availability.controller')(availabilityManager);
 // filling collections with dummy data
-for (let i = 1; i < 101; ++i) {
+for (var i = 1; i < 101; ++i) {
     productManager.create({
         id: i,
-        name: `Product-${i}`
-    } as Product)
+        name: "Product-" + i
+    });
 }
-const products: Product[] = productManager.findAll();
-const now = new Date().getTime();
-for (let d = 0; d < 90; ++d) { //For every day in the last 3 months, set each product availability
-
-    for (let p of products) {
+var products = productManager.findAll();
+var now = new Date().getTime();
+for (var d = 0; d < 90; ++d) { //For every day in the last 3 months, set each product availability
+    for (var _i = 0, products_1 = products; _i < products_1.length; _i++) {
+        var p = products_1[_i];
         availabilitiesCol.insert({
             productId: p.id,
             productName: p.name,
             date: new Date(new Date().setTime(now - d * 8.64e+7)),
-            inventoryLevel: Math.floor(Math.random()*60)+20
-        } as Availability)
+            inventoryLevel: Math.floor(Math.random() * 60) + 20
+        });
     }
 }
-
 // initializing MVC app
 var app = Express();
-
 // view engine setup
 app.set('views', Path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
-
 // define folder for static files
-app.use(Express.static(Path.join(__dirname, 'wwwroot')));
-
+app.use(Express.static(Path.join(__dirname, '/../Flowlity.GUI/wwwroot')));
 // allows use of application/json
 app.use(Express.json());
-
 // defining routes
 app.use('/products', productController);
 app.use('/availabilities', availabilityController);
-app.use('/', routes);
-
+app.use('/', index_1.default);
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
     var err = new Error('Not Found');
     err['status'] = 404;
     next(err);
 });
-
 // error handlers
-
 // development error handler
 // will print stacktrace
 if (app.get('env') === 'development') {
-    app.use((err: any, req, res, next) => {
+    app.use(function (err, req, res, next) {
         res.status(err['status'] || 500);
         res.render('error', {
             message: err.message,
@@ -78,19 +66,17 @@ if (app.get('env') === 'development') {
         });
     });
 }
-
 // production error handler
 // no stacktraces leaked to user
-app.use((err: any, req, res, next) => {
+app.use(function (err, req, res, next) {
     res.status(err.status || 500);
     res.render('error', {
         message: err.message,
         error: {}
     });
 });
-
 app.set('port', process.env.PORT || 3000);
-
 var server = app.listen(app.get('port'), function () {
     Debug('Express server listening on port ' + server.address().port);
 });
+//# sourceMappingURL=app.js.map
--- Flowlity.Core/app.ts
+++ Flowlity.Core/app.ts
@@ -1,64 +1,76 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-//initializing in-memory database and collections
-var Loki = require("lokijs");
-var Path = require("path");
-var Debug = require("debug");
-var Express = require("express");
-var index_1 = require("../Flowlity.Core/routes/index");
+﻿//initializing in-memory database and collections
+import * as Loki from 'lokijs';
+import * as Path from 'path';
+import { Product } from "./BO/product";
+import { Availability } from "./BO/availability";
+import * as Debug from 'debug';
+import * as Express from 'express';
+import routes from './routes/index';
+
 var inMemoryDb = new Loki('data.json');
+
 var productsCol = inMemoryDb.addCollection('products');
-var productRepository = require('../Flowlity.Core/DAL/product.repository')(productsCol);
-var productManager = require('../Flowlity.Core/BLL/product.manager')(productRepository);
-var productController = require('../Flowlity.Core/Controller/product.controller')(productManager);
+var productRepository = require( './DAL/product.repository')(productsCol);
+var productManager = require('./BLL/product.manager')(productRepository);
+var productController = require('./Controller/product.controller')(productManager);
+
 var availabilitiesCol = inMemoryDb.addCollection('availabilities');
-var availabilityRepository = require('../Flowlity.Core/DAL/availability.repository')(availabilitiesCol);
-var availabilityManager = require('../Flowlity.Core/BLL/availability.manager')(availabilityRepository);
-var availabilityController = require('../Flowlity.Core/Controller/availability.controller')(availabilityManager);
+var availabilityRepository = require('./DAL/availability.repository')(availabilitiesCol);
+var availabilityManager = require('./BLL/availability.manager')(availabilityRepository);
+var availabilityController = require('./Controller/availability.controller')(availabilityManager);
+
 // filling collections with dummy data
-for (var i = 1; i < 101; ++i) {
+for (let i = 1; i < 101; ++i) {
     productManager.create({
         id: i,
-        name: "Product-" + i
-    });
+        name: `Product-${i}`
+    } as Product)
 }
-var products = productManager.findAll();
-var now = new Date().getTime();
-for (var d = 0; d < 90; ++d) { //For every day in the last 3 months, set each product availability
-    for (var _i = 0, products_1 = products; _i < products_1.length; _i++) {
-        var p = products_1[_i];
+const products: Product[] = productManager.findAll();
+const now = new Date().getTime();
+for (let d = 0; d < 90; ++d) { //For every day in the last 3 months, set each product availability
+
+    for (let p of products) {
         availabilitiesCol.insert({
             productId: p.id,
             productName: p.name,
             date: new Date(new Date().setTime(now - d * 8.64e+7)),
-            inventoryLevel: Math.floor(Math.random() * 60) + 20
-        });
+            inventoryLevel: Math.floor(Math.random()*60)+20
+        } as Availability)
     }
 }
+
 // initializing MVC app
 var app = Express();
+
 // view engine setup
 app.set('views', Path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
+
 // define folder for static files
-app.use(Express.static(Path.join(__dirname, 'wwwroot')));
+app.use(Express.static(Path.join(__dirname, '/../Flowlity.GUI/wwwroot')));
+
 // allows use of application/json
 app.use(Express.json());
+
 // defining routes
 app.use('/products', productController);
 app.use('/availabilities', availabilityController);
-app.use('/', index_1.default);
+app.use('/', routes);
+
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
     var err = new Error('Not Found');
     err['status'] = 404;
     next(err);
 });
+
 // error handlers
+
 // development error handler
 // will print stacktrace
 if (app.get('env') === 'development') {
-    app.use(function (err, req, res, next) {
+    app.use((err: any, req, res, next) => {
         res.status(err['status'] || 500);
         res.render('error', {
             message: err.message,
@@ -66,17 +78,19 @@ if (app.get('env') === 'development') {
         });
     });
 }
+
 // production error handler
 // no stacktraces leaked to user
-app.use(function (err, req, res, next) {
+app.use((err: any, req, res, next) => {
     res.status(err.status || 500);
     res.render('error', {
         message: err.message,
         error: {}
     });
 });
+
 app.set('port', process.env.PORT || 3000);
+
 var server = app.listen(app.get('port'), function () {
     Debug('Express server listening on port ' + server.address().port);
 });
-//# sourceMappingURL=app.js.map
